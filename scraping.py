9b0284b02a172573579bf88d7a92421ca447ddc1
mohameddwedar689 <mohameddwedar689@gmail.com>
2022-11-22 20:05:38 +0200

    create scraping file

---
+++ scraping.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+from itertools import zip_longest
+
+
+# use requests to fetch the url
+result = requests.get("https://wuzzuf.net/search/jobs/?q=python&a=hpb")
+
+# save page content
+src = result.content
